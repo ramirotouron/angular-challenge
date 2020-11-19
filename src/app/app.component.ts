9234a51e6ac6778d37aa5ad1f1a5d402c46961c9
Rami <rtouron@ignetworks.com>
2020-11-19 17:15:15 -0300

    challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'entrevista';
+  
+  ballMatrixPrev: [boolean, boolean, boolean, boolean] = [false, false, false, false];
+  ballMatrix: [boolean, boolean, boolean, boolean] = [true, false, false, false];
+
+  clickedBox(pos: number) {
+    
+    this.ballMatrixPrev = [...this.ballMatrix]
+    const lastPos = this.ballMatrix.findIndex(box => box == true)
+    this.ballMatrix[lastPos] = false
+    this.ballMatrix[pos] = true
+
+  }
 }
